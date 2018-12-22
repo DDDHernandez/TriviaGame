6746fa4f0ac69271e369b5179adcdf97a68453b9
Dylon <Dielonh@gmail.com>
2018-12-22 14:13:24 -0600

    User answers id and comparing them to correct answers id

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,43 @@
 $(document).ready(function() {
 
+
+    var number = 20;
+    var intervalId;
         $(".btn").on("click", function() {
-           $('#Start-button').replaceWith("#hiddenQuestions");
-            $("#hiddenQuestions").css("display", "block");
-          })
+            console.log($('#Start-button'))
+           $('#Start-button').css("display", "none");
+            $("#gameStart").css("display", "block");
+            run();
+        });
+     
+
+            function run() {
+                clearInterval(intervalId);
+                intervalId = setInterval(decrement, 1000);
+              } 
+              function decrement() {
+                number--;
+                $("#showNumber").html("<h2>" + number + "</h2>");  
+                };
+                   
+                if (number === 0) {
+                    stop();
+                    alert("Time Up!");
+                    Answers();
+                    $("#hiddenQuestions").html("Congratulations you got " + correct + " out of 5 correct!\nRefresh the page to play again!" )
+                } else {
+                    showQuestion();
+                    run();
+                }
+                function stop() {
+                    clearInterval(intervalId);
+                  }
+                  var correct = 0;
+               
+            function Answers() {
+                if ("#yes" == checked){
+                    correct++;}
+                
+            }
+           
 })
