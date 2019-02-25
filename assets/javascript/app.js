c31013e7a9b348aa2ca48d8652b0d9e1b68560df
Dylon <Dielonh@gmail.com>
2019-02-24 21:49:02 -0600

    made changes to index.html

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
 
 
-    var number = 20;
+    var number = 15;
     var intervalId;
-        $(".btn").on("click", function() {
+        $("#Start").on("click", function() {
             console.log($('#Start-button'))
            $('#Start-button').css("display", "none");
             $("#gameStart").css("display", "block");
@@ -16,31 +16,32 @@ $(document).ready(function() {
               } 
               function decrement() {
                 number--;
-                $("#showNumber").html("<h2>" + number + "</h2>");  
+                $("#showNumber").html("<h2>" + number + "</h2>"); 
+                
                    
                 if (number === 0) {
-                stop();
-                alert("Time Up!");
+                Results();
+              //  alert("Time Up!");
                  console.log("Time up!");
                  
-                }
-            } 
-              function stop() {
-                    clearInterval(intervalId);
-                    Answers();
-
-              
-                 var correct = 0;
                
+              
+              function Results() {
+                    Answers();               
               function Answers() {
-             if ($('#yes').attr('checked') === 'checked') {
-                  correct++;
-                  $("#gameStart").css("display", "none");   
+                 correct = [];
+             //   $('form').each(function() {
+            //var result = document.radioForm.optradio.class;
+                 if (('#yes').attr('checked') === 'checked') {
+               correct++;
+                };
+            };
+            
+             $("#gameStart").css("display", "none");   
               $("#results").html("Congratulations you got " + correct + " out of 7 correct!\nRefresh the page to play again!" )
                 console.log(correct)
-            }
-        }; 
-        }
-    });
-           
+                    };
+            };
+        });
+                   
 
